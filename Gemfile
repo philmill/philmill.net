2f4a831ef20fd5a6cfc133ba3882f5a14578d342
philmill <phil@philmill.net>
2014-02-27 20:15:02 -0500

    Run Unicorn in production and use standard Bundle gem location for deployments.

--- Gemfile
+++ Gemfile
@@ -4,19 +4,19 @@ gem 'nesta', git: 'git://github.com/philmill/nesta.git'
 #gem 'nesta', path: '/home/philmill/webDev/repos/nesta'
 gem 'redcarpet', '~>3.0'
 gem 'dropbox-sdk', '~>1.6.3'
-#gem 'oj', '~>2.5'
 gem 'sinatra-asset-pipeline', '0.3.3'
 gem 'uglifier', '~>2.4.0'
 gem 'compass', '~>0.12.2'
 gem 'susy', '~>1.0'
 #gem 'astro_moon', '~> 0.2'
-
-#gem 'rack-coffee', '~>1.0'
+#gem 'oj', '~>2.5'  # keep in case we need some fast JSON
 
 group :development do
-  #gem 'thin', '~>1.6'
-  #gem 'shotgun', '~>0.9'
   gem 'mr-sparkle', '~>0.3.0'
-  gem 'capistrano'
+  gem 'capistrano', '~>2.15.5'
   gem 'rvm-capistrano'
+end
+
+group :production do
+  gem 'unicorn', '~>4.8'
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -3,14 +3,8 @@ require 'bundler/capistrano'
 require 'capistrano/ext/multistage'
 
 # tell rvm which ruby to run
-set :rvm_ruby_string, 'ruby-2.0.0-p247'
+set :rvm_ruby_string, 'ruby-2.1.1@philmill'
 set :rvm_type, :user
-
-# set bundle executible to the global gemset
-set :bundle_cmd, '/home/philmill/.rvm/gems/ruby-2.0.0-p247@global/bin/bundle'
-# set bundle deployment location to a common directory for sharing gems between projects
-# this also allows us to use thin as an OS service (rvm wrapper points here) as well as start and stop individual projects as needed
-set :bundle_dir, '/home/philmill/.shared_bundle'
 
 set :stages, %w(staging production)
 set :default_stage, 'production'
@@ -31,23 +25,22 @@ set :branch, 'master'
 set :use_sudo, false
 set :ssh_options, { :forward_agent => true }
 
-set :copy_exclude, %w(.git/ themes/philmill/src)
+set :copy_exclude, %w(.git/ assets/ themes/philmill/src)
 
 set :keep_releases, 3
 
 namespace :deploy do
 
   task :start, :roles => [:app, :web] do
-    run "cd #{deploy_to}/current && nohup bundle exec thin -C /etc/thin/philmill.yml start"
+    run "service unicorn-pm start"
   end
 
   task :stop, :roles => [:app, :web] do
-    run "cd #{deploy_to}/current && nohup bundle exec thin -C /etc/thin/philmill.yml stop"
+    run "service unicorn-pm stop"
   end
 
   task :restart, :roles => [:app, :web] do
-    deploy.stop
-    deploy.start
+    run "service unicorn-pm restart"
   end
 
   task :cold do
